e1a5ea9d0e1026c7645cb62da300177427d1bc11
Yuncong Zhang <yczhangsjtu@gmail.com>
2023-08-13 12:16:24 +0800

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# mytex
-LaTeX project management tool.
+# The mytex Tool
+LaTeX project management tool. Provides functionalities including:
+
+- Automatically create new LaTeX projects using given templates. This tool already includes some templates such as Beamer, LNCS, ACM.
+- Switch a project to a different template.
+- Manage the self-defined latex commands.
+- Define custom grammar and replace automatically.
+- Detect citations in tex files and manage the bib file, select which fields to include in the bib items.
+
+All the configuration files will be stored in a `.mytex` folder inside the project directory. The configuration files are in yaml format.
+
+## Initialize the tool
+
+If `mytex` has never been executed before, executing it the first time will create a `mytex` directory in the `$HOME/.config` folder. The folder structure will be as follows, where the contents of the templates are omitted for clarity.
+
+```
+$HOME/.config/mytex
+- config.yaml
+- templates/
+  - article/
+  - acm/
+  - beamer/
+  - lncs/
+```
+
+## Create a new project
+
+Execute the following command where you want to place the project.
+
+```bash
+mytex create
+```
+
+The program will ask a few questions.
+
+```
+Project name: Hello
+Select template:
+(1) Empty article
+(2) LNCS
+(3) ACM
+(4) Beamer
+Your choice: 1
+Title: Hello World
+```
+
+Then create a directory with a `.mytex` folder in it, and the template is already applied. The new project will have the following structure.
+
+```
+Hello
+- .mytex/
+	- config.yaml
+- main.tex
+- packages.tex
+- definitions.tex
+- body.tex
+```
+
+## Configuration file
+
+The default configuration file will look as follows.
+
+```yaml
+- name: Hello
+- template: article
+- title: Hello World
+- subtitle: None
+- date: May 1, 2023
+- authors: None
+- keywords: None
+```
+
+## Switch template
+
+Execute the following command inside the project.
+
+```
+mytex template
+```
+
+The program will try to find a `.mytex` folder in the current directory, or ancestor directories. If succeeds, it asks the user to choose a template, then replaces all the template files with those in the selected template.
---
+++ mytex.py
@@ -0,0 +1,111 @@
+#!/usr/local/bin/python
+
+import os
+import yaml
+import logging
+import shutil
+import sys
+
+
+class Mytex:
+    def __init__(self):
+        self.config_path = os.path.join(os.path.expanduser("~"), ".config", "mytex", "config.yaml")
+        self.templates_dir = os.path.join(os.path.expanduser("~"), ".config", "mytex", "templates")
+        if not os.path.exists(self.templates_dir):
+            script_path = os.path.realpath(__file__)
+            if os.path.islink(script_path):
+                script_path = os.path.realpath(os.readlink(script_path))
+            shutil.copytree(os.path.join(os.path.dirname(script_path), "templates"), self.templates_dir)
+        self.config = self._read_config(self.config_path)
+        self.templates = self._get_templates()
+
+    def _read_config(self, config_path):
+        config_dir = os.path.dirname(config_path)
+        if not os.path.exists(config_dir):
+            os.makedirs(config_dir)
+
+        if not os.path.exists(config_path):
+            with open(config_path, "w") as f:
+                yaml.safe_dump({}, f, indent=4)
+
+            config = {}
+        else:
+            with open(config_path, "r") as f:
+                config = yaml.safe_load(f)
+
+        return config
+
+    def _get_templates(self):
+        templates = {}
+        for filename in os.listdir(self.templates_dir):
+            template_name = os.path.splitext(filename)[0]
+            templates[template_name] = os.path.join(self.templates_dir, filename)
+        return templates
+
+    def create(self):
+        project_name = input("Project name: ")
+        template_names = list(self.templates.keys())
+        print("Available templates:")
+        for index, template_name in enumerate(template_names):
+            print("[{}] {}".format(index + 1, template_name))
+
+        template_index = int(input("Select template (enter number): ")) - 1
+        template_name = template_names[template_index]
+
+        if template_name not in self.templates:
+            raise ValueError("Invalid template name: {}".format(template_name))
+
+        project_dir = os.path.join(os.getcwd(), project_name)
+        if os.path.exists(project_dir):
+            raise ValueError("Project already exists: {}".format(project_dir))
+
+        os.makedirs(project_dir)
+        self._copy_template(project_dir, template_name)
+        self._update_config(project_dir, project_name, template_name)
+
+    def template(self, project_dir):
+        if not os.path.exists(project_dir):
+            raise ValueError("Project does not exist: {}".format(project_dir))
+
+        config = self._read_config(os.path.join(project_dir, ".mytex", "config.yaml"))
+        template_name = config["template"]
+        self._copy_template(project_dir, template_name)
+
+    def _copy_template(self, project_dir, template_name):
+        template_path = self.templates[template_name]
+        for filename in os.listdir(template_path):
+            source_path = os.path.join(template_path, filename)
+            destination_path = os.path.join(project_dir, filename)
+            shutil.copy(source_path, destination_path)
+
+    def _update_config(self, project_dir, project_name, template_name):
+        config = self._read_config(os.path.join(project_dir, ".mytex", "config.yaml"))
+        config["name"] = project_name
+        config["template"] = template_name
+        with open(os.path.join(project_dir, ".mytex", "config.yaml"), "w") as f:
+            yaml.safe_dump(config, f, indent=4)
+
+
+if __name__ == "__main__":
+    m = Mytex()
+
+    if len(sys.argv) == 1:
+        # No command is given, so print help message
+        print("Usage: mytex <command> [options]")
+        print("Available commands:")
+        print("  create   Create a new project")
+        print("  template Switch the template of an existing project")
+        print("  help     Print this help message")
+
+    elif sys.argv[1] == "create":
+        # Create a new project
+        m.create()
+
+    elif sys.argv[1] == "template":
+        # Switch the template of an existing project
+        m.template(sys.argv[2])
+
+    else:
+        # Invalid command
+        print("Invalid command: {}".format(sys.argv[1]))
+        print("Use `mytex help` to see a list of available commands.")
